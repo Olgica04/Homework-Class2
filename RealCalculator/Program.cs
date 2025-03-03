3714f1280c1628fc0878ba468ce383183006d5b4
Olgica04 <olgicatrpcevska@gmail.com>
2025-03-03 16:42:31 +0100

    Add project files.

---
+++ RealCalculator/Program.cs
@@ -0,0 +1,38 @@
+﻿Console.Write("Enter your first number: ");
+string input1 = Console.ReadLine();
+//int firstNumber = Convert.ToInt32(input1);
+bool succes1= int.TryParse(input1, out int firstNumber);
+
+Console.Write("Enter your second number: ");
+string input2 = Console.ReadLine();
+//int secondnumber = Convert.ToInt32(input2);
+bool succes2 = int.TryParse(input2, out int secondNumber);
+
+Console.Write("Enter your operation: ");
+string operation = Console.ReadLine();
+
+int result;
+
+switch (operation)
+{
+    case "+":
+        result = firstNumber + secondNumber;
+        Console.WriteLine("The result is: " + result);
+        break;
+    case "-":
+        result = firstNumber - secondNumber;
+        Console.WriteLine("The result is: " + result);
+        break;
+    case "*":
+        result = firstNumber * secondNumber;
+        Console.WriteLine("The result is: " + result);
+        break;
+    case "/":
+        result = firstNumber / secondNumber;
+        Console.WriteLine("The result is: " + result);
+        break;
+    default:
+        Console.WriteLine("Invalid input");
+        break;
+}
+
---
+++ SwapNumbers/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Input the First Number: ");
+string input1 = Console.ReadLine();
+bool succes1 = int.TryParse(input1, out int firstNumber);
+
+Console.Write("Input the Second number: ");
+string input2 = Console.ReadLine();
+bool succes2 = int.TryParse(input2, out int secondNumber);
+
+int temp;
+
+temp = firstNumber;
+firstNumber = secondNumber;
+secondNumber = temp;
+
+Console.WriteLine("After swaping: ");
+Console.WriteLine("First Number: " + firstNumber);
+Console.WriteLine("Second Number: " + secondNumber);
